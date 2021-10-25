900a2fccce7f3d31846a6147cca2efc974bc12e3
Jon <jfrancois@synbioz.com>
2021-10-25 11:02:29 +0200

    feat: can create user with custom attributes

--- lib/keycloak-admin/client/user_client.rb
+++ lib/keycloak-admin/client/user_client.rb
@@ -6,8 +6,8 @@ module KeycloakAdmin
       @realm_client = realm_client
     end
 
-    def create!(username, email, password, email_verified, locale)
-      user = save(build(username, email, password, email_verified, locale))
+    def create!(username, email, password, email_verified, locale, attributes = {})
+      user = save(build(username, email, password, email_verified, locale, attributes))
       search(user.email)&.first
     end
 
@@ -183,13 +183,13 @@ module KeycloakAdmin
 
     private
 
-    def build(username, email, password, email_verified, locale)
+    def build(username, email, password, email_verified, locale, attributes)
       user                     = UserRepresentation.new
       user.email               = email
       user.username            = username
       user.email_verified      = email_verified
       user.enabled             = true
-      user.attributes          = {}
+      user.attributes          = attributes
       user.attributes[:locale] = locale if locale
       user.add_credential(CredentialRepresentation.from_password(password))
       user
